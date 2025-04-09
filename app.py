299081aa44cfaa64c4b7a8506a948cc5a3e3d883
Hansie Kristi <iamhansie@gmail.com>
2025-04-09 05:28:27 +0100

    Updated mdoel

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from PIL import Image
 
 from unet_model import unet_model
 
-input_shape = (112, 112, 3)
-model_weights_path = "unet_best.weights.h5"
+# input_shape = (112, 112, 3)
+model_weights_path = "optimized_unet_checkpoint.keras"
 
-def load_model_and_weights(input_shape, model_weights_path):
-    model = unet_model(input_shape)
+def load_model_and_weights(model_weights_path):
+    model = unet_model()
     model.load_weights(model_weights_path)
     return model
 
@@ -26,13 +26,13 @@ def main():
 
     if st.button('Predict'):
         if uploaded_image is not None:
-            model = load_model_and_weights(input_shape, model_weights_path)
+            model = load_model_and_weights(model_weights_path)
             preprocessed_image = preprocess_image(uploaded_image)
             prediction = model.predict(preprocessed_image)
 
             col1, col2 = st.columns(2)
-            col1.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
-            col2.image(prediction, caption='Predicted Image', use_column_width=True)
+            col1.image(uploaded_image, caption='Uploaded Image', use_container_width=True)
+            col2.image(prediction, caption='Predicted Image', use_container_width=True)
         else:
             st.warning('Please upload an image.')
 
